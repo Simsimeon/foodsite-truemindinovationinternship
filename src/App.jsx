17446715e16ff79f458056b49d9a76a044b26a6c
unknown <blissimeonx@gmail.com>
2026-02-27 22:43:49 +0100

    optimized to reduce load time

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Home from './page/Home'
-import Explore from './page/Expore'
-import Myorder from './page/Myorder'
-import Account from './page/Account'
- import Login from './page/Login'
- import { GlobalStyles } from './styles/GlobalStyles'
-import AppLayout from './ui/AppLayout'
+// import Home from './page/Home'
+// import Explore from './page/Expore'
+// import Myorder from './page/Myorder'
+// import Account from './page/Account'
+//  import Login from './page/Login'
+//  import { GlobalStyles } from './styles/GlobalStyles'
+// import AppLayout from './ui/AppLayout'
+import { lazy } from 'react'
+const Homepage=lazy(()=>import("./page/Home"))
+const Explorepage=lazy(()=>import("./page/Explore"))
+const Accountpage=lazy(()=>import("./page/Account"))
+const Myorderage=lazy(()=>import("./page/Myorder"))
+const Loginpage=lazy(()=>import("./page/Login"))
 
 const App = () => {
   return (
@@ -19,13 +25,13 @@ const App = () => {
    <Routes>
     <Route element={<AppLayout/>}>
     <Route index element={<Navigate replace to="home" />} />
-       <Route  path='home' element={<Home/>}/>
-     <Route path='explore' element={<Explore/>}/>
-     <Route path='myorder' element={<Myorder/>}/>
-     <Route path='account' element={<Account/>}/> 
+       <Route  path='home' element={<Homepage/>}/>
+     <Route path='explore' element={<Explorepage/>}/>
+     <Route path='myorder' element={<Myorderpage/>}/>
+     <Route path='account' element={<Accountpage/>}/> 
      
     </Route>
-     <Route path='login' element={<Login/>}/>
+     <Route path='login' element={<Loginpage/>}/>
     {/* <Route index element={<Navigate replace to="home" />} /> */}
     
     
